d299dde0970e1030f49332aa4b235692ecf7d06b
KarinaI <7karina.ibr@gmail.com>
2024-01-20 17:14:52 +0300

    feat: solve 02-write-file and 03-files-in-folder

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+
+fs.writeFile(
+  path.join(__dirname, "text.txt"),
+  '',
+  () => {
+  console.log("Hello!");
+  }
+);
+
+const { stdout, stdin, exit } = process;
+function writeBye() {
+  stdout.write("\nGoodbye");
+  exit();
+}
+stdout.write("Write down your text:\n");
+stdin.on("data", (data) => {
+  if (data.toString() === 'exit\n') {
+    writeBye();
+  }
+  fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+    if (err) throw err;
+    fs.readFile(
+      path.join(__dirname, "text.txt"),
+      "utf-8",
+      (err) => {
+        if (err) throw err;
+      },
+    );
+    },
+  );
+});
+
+process.on("SIGINT", writeBye);
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const newPath = path.join(__dirname, "secret-folder");
+
+fs.promises.readdir(
+  newPath,
+  {withFileTypes: true}
+).then(
+  (data) => {
+  for (let el of data) {
+    if (el.isFile()) {
+    const filePath = path.join(newPath, el.name);
+    fs.promises.stat(filePath).then((info) => {
+      console.log(`${el.name.slice(0, el.name.indexOf("."))} - ${path.extname(el.name).slice(1)} - ${info.size/1024} kb`);
+    });
+    }
+  }
+  }
+)
