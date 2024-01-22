1bd1cafab8c0c75e6f6de5aca0b9f6274d45df23
KarinaI <7karina.ibr@gmail.com>
2024-01-22 21:32:29 +0300

    refactor: refactor prettier 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,36 +1,26 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-
-fs.writeFile(
-  path.join(__dirname, "text.txt"),
-  '',
-  () => {
-  console.log("Hello!");
-  }
-);
+fs.writeFile(path.join(__dirname, 'text.txt'), '', () => {
+  console.log('Hello!');
+});
 
 const { stdout, stdin, exit } = process;
 function writeBye() {
-  stdout.write("\nGoodbye");
+  stdout.write('\nGoodbye');
   exit();
 }
-stdout.write("Write down your text:\n");
-stdin.on("data", (data) => {
+stdout.write('Write down your text:\n');
+stdin.on('data', (data) => {
   if (data.toString() === 'exit\n') {
     writeBye();
   }
-  fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
     if (err) throw err;
-    fs.readFile(
-      path.join(__dirname, "text.txt"),
-      "utf-8",
-      (err) => {
-        if (err) throw err;
-      },
-    );
-    },
-  );
+    fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err) => {
+      if (err) throw err;
+    });
+  });
 });
 
-process.on("SIGINT", writeBye);
+process.on('SIGINT', writeBye);
