ee2b3ec71471bed1ddad47f360aabcf1e30df8e1
KarinaI <7karina.ibr@gmail.com>
2024-01-22 21:30:32 +0300

    refactor: refactor prettier 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,19 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const newPath = path.join(__dirname, "secret-folder");
+const newPath = path.join(__dirname, 'secret-folder');
 
-fs.promises.readdir(
-  newPath,
-  {withFileTypes: true}
-).then(
-  (data) => {
-    for (let el of data) {
-      if (el.isFile()) {
+fs.promises.readdir(newPath, { withFileTypes: true }).then((data) => {
+  for (let el of data) {
+    if (el.isFile()) {
       const filePath = path.join(newPath, el.name);
       fs.promises.stat(filePath).then((info) => {
-        console.log(`${el.name.slice(0, el.name.indexOf("."))} - ${path.extname(el.name).slice(1)} - ${info.size} b`);
+        console.log(
+          `${el.name.slice(0, el.name.indexOf('.'))} - ${path
+            .extname(el.name)
+            .slice(1)} - ${info.size} b`,
+        );
       });
-      }
     }
   }
-)
+});
