e86cd39530979bfb39ab0d15607b65dc35594dd6
KarinaI <7karina.ibr@gmail.com>
2024-01-22 22:28:51 +0300

    refactor: apply prettier 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,23 +4,23 @@ const path = require('path');
 (async function writeFile() {
   await fs.promises.writeFile(path.join(__dirname, 'text.txt'), '');
 
-const { stdout, stdin, exit } = process;
-function writeBye() {
-  stdout.write('\nGoodbye');
-  exit();
-}
-stdout.write('Hello! Write down your text:\n');
-stdin.on('data', (data) => {
-  if (data.toString() === 'exit\n') {
-    writeBye();
+  const { stdout, stdin, exit } = process;
+  function writeBye() {
+    stdout.write('\nGoodbye');
+    exit();
   }
-  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
-    if (err) throw err;
-    fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err) => {
+  stdout.write('Hello! Write down your text:\n');
+  stdin.on('data', (data) => {
+    if (data.toString() === 'exit\n') {
+      writeBye();
+    }
+    fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
       if (err) throw err;
+      fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err) => {
+        if (err) throw err;
+      });
     });
   });
-});
 
-process.on('SIGINT', writeBye);
-})()
+  process.on('SIGINT', writeBye);
+})();
